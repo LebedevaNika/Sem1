5de94e9b3476336ef005f786fdce627e49bdfa5f
LebedevaNika <lnsnika@yandex.ru>
2025-10-29 23:17:59 +0300

    Create bubble_sort.cpp

---
+++ src/bubble_sort.cpp
@@ -0,0 +1,17 @@
+#include "../include/bubble_sort.h"
+
+template<typename T>
+void bubbleSort(T arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                T temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+template void bubbleSort<int>(int arr[], int n);
+template void bubbleSort<double>(double arr[], int n);
